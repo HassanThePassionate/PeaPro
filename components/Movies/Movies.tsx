f85c8ce0b0a34e6ea1820cdabbcbd3aea4e1fe1e
HassanThePassionate <maqsoodia777@gmail.com>
2024-06-12 17:33:06 +0500

    Update

--- components/Movies/Movies.tsx
+++ components/Movies/Movies.tsx
@@ -13,8 +13,8 @@ const Movies = ({ read, padding }: MovieProps) => {
       <div
         className={`bg-white dark:bg-[#43444A] rounded-[16px] py-[32px] min-[400px]:grid-cols-3 grid-cols-2 gap-2 ${padding} grid sm:grid-cols-5 sm:gap-4 lg:grid-cols-6 lg:gap-x-[14px] lg:gap-y-[16px] xl:grid-cols-7 xl:gap-x-[34px] xl:gap-y-[24px]`}
       >
-        {data.map((movie) => (
-          <MovieCard key={movie.title} {...movie} size={80} />
+        {data.map((movie, i) => (
+          <MovieCard key={i} {...movie} size={80} />
         ))}
       </div>
     </>
